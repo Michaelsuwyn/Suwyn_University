6017396658e4bc4289392bbb6b79d9afa311e366
Michael <suwyn45@gmail.com>
2018-02-07 06:44:05 -0700

    carousel working

--- src/app/carousel.component.ts
+++ src/app/carousel.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'carousel',
-    templateUrl: './carousel.component.html'
+    templateUrl: './carousel.component.html',
+    styleUrls: ['./carousel.component.css'],
 
 })
 export class CarouselComponent {
@@ -11,16 +12,17 @@ export class CarouselComponent {
   
     constructor() {
       this.items = [
-        { name: 'assets/carouselimages/suwynstate.jpg' },
+        { name: 'assets/carouselimages/suwynstate.jpg',},
         { name: 'assets/carouselimages/footballstate.jpg' },
         { name: 'assets/carouselimages/hockeystate.jpg' },
         { name: 'assets/carouselimages/ridgestate.jpg' },
         { name: 'assets/carouselimages/suwynmoonvalley.jpg' },
-        { name: 'assets/carouselimages/suwynstate.jpg' },
-        { name: 'assets/carouselimages/footballstate.jpg' },
-        { name: 'assets/carouselimages/hockeystate.jpg' },
-        { name: 'assets/carouselimages/ridgestate.jpg' },
-        { name: 'assets/carouselimages/suwynmoonvalley.jpg' },
+        { name: 'assets/carouselimages/one.jpg' },
+        { name: 'assets/carouselimages/two.jpg' },
+        { name: 'assets/carouselimages/three.jpg' },
+        { name: 'assets/carouselimages/four.jpg' },
+        { name: 'assets/carouselimages/five.jpg' },
+        
         
       ]
     }
